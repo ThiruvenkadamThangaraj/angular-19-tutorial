d82e4e6624f6ce55aa6e50d660cfd83665ed8c52
ThiruvenkadamThangaraj <thiru.karur@gmail.com>
2025-06-08 15:31:44 -0400

    angular learning

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./login/login";
+import { Signup } from "./signup/signup";
+import { Profile } from "./profile/profile";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Login, Signup, Profile],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected title = 'angular-19-tutorial';
+  name: string = "Thiruvenkadam";
+  data3: number = 300;
+  data4:any;
+  
+  userLogin: boolean = false;
+   handClickEvent() {
+    //alert('Button Clicked!');
+    console.log('Button Clicked!');
+    this.otherFunction();
+   
+  }
+  otherFunction() {
+  
+    console.log('Other Function Called!');
+    console.log(this.name);
+    console.log(this.userLogin);
+    this.data4 = 500;
+    console.log('data4=400 = '+this.data4);
+    this.data4 = "Hello World!";
+   console.log('data4=Hello World!='+this.data4);
+  }
+  protected title = 'angular-19-tutorial Studied by Thiru';
+ 
+  hello= 'Hello World!';
+  data =100;
+  data1 = 200;
+
+  sum(a: number, b: number) {
+    console.log(a+b);
+    return a + b;
+  }
 }
---
+++ src/app/profile/profile.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'profile',
+  //template:"<div>Profile Component</div>",
+    templateUrl: './profile.html',
+  //styles:"div { color: blue; }"
+    styleUrl: './profile.css'
+})
+export class Profile {
+
+
+}
