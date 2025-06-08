c6db80f89d525ad4b320ab63e43df226919c6aa7
ThiruvenkadamThangaraj <thiru.karur@gmail.com>
2025-06-08 16:14:59 -0400

    functions

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,35 +11,58 @@ import { Profile } from "./profile/profile";
   styleUrl: './app.css'
 })
 export class App {
+  count: number = 0;
   name: string = "Thiruvenkadam";
   data3: number = 300;
-  data4:any;
-  
+  data4: any;
+
   userLogin: boolean = false;
-   handClickEvent() {
+  handClickEvent() {
     //alert('Button Clicked!');
     console.log('Button Clicked!');
     this.otherFunction();
-   
+
   }
   otherFunction() {
-  
+
     console.log('Other Function Called!');
     console.log(this.name);
     console.log(this.userLogin);
     this.data4 = 500;
-    console.log('data4=400 = '+this.data4);
+    console.log('data4=400 = ' + this.data4);
     this.data4 = "Hello World!";
-   console.log('data4=Hello World!='+this.data4);
+    console.log('data4=Hello World!=' + this.data4);
   }
   protected title = 'angular-19-tutorial Studied by Thiru';
- 
-  hello= 'Hello World!';
-  data =100;
+
+  hello = 'Hello World!';
+  data = 100;
   data1 = 200;
 
   sum(a: number, b: number) {
-    console.log(a+b);
+    console.log(a + b);
     return a + b;
+  }
+  handleIncrement() {
+    this.count++;
+    console.log('Incremented Count: ' + this.count);
+  }
+  handleDecrement() {
+    this.count--;
+    console.log('Decremented Count: ' + this.count);
+  }
+  resetCount() {
+    this.count = 0;
+    console.log('Count Reset to: ' + this.count);
+  }
+
+  handleCounterChange(value: string) {
+    if (value == 'minus') {
+      this.count--;
+    } else if (value == 'plus') {
+      this.count++;
+    } else if (value == 'reset') {
+      this.count = 0;
+    }
   }
 }
